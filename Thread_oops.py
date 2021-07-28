182c5f9cfe92a183919676fdbb0484c6626e1c26
Sathish Kumar <chesathish5201@gmail.com>
2021-07-28 19:52:59 +0530

    Receive messages from client

--- Thread_oops.py
+++ Thread_oops.py
@@ -14,8 +14,41 @@ class Process(Thread):
     
     def run(self):
             print("\n",current_thread(),addr[0])
-            conn.close()
+            #presentation layer
+            conn.send("Welcome to Suckers show type what you want to say: \n".encode())
+            #print the messages from client using with list 
+            stack = []
+            while True:
+                try:
+                    recv = conn.recv(4096)
+                    recv = recv.decode()
+                    #close the connection if client sends Hex value ctrl+q
+                    if recv == '\x11':
+                        conn.close()
+                except OSError:
+                    print("connection closed in this thread {}".format(addr[0]))
+                    break
 
+                while True:
+                    #It appends the character to stack
+                    stack.append(recv)
+
+                    #It prints the stack after client clicks enter
+                    if recv == '\r\n':
+                        #Convert list to string function  
+                        stack_part = stack 
+                        def listtostr(stack_part):
+                            str = ""
+                            for i in stack_part:
+                                str+=i
+                            return str
+                        stack_part = listtostr(stack_part)
+                        print(stack_part)
+                        break
+                    break
+
+
+#session layer
 sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 sock.bind(("localhost",3074))
@@ -23,7 +56,8 @@ sock.listen()
 print("listening...")
 while True:
     conn,addr = sock.accept()
+    #New Thread started
     Thread_Start(conn,addr)
     print("started...",addr[0])
-
+    break
 sock.close()
